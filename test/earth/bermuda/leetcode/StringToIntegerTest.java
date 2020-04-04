a7fd212ff743f18160743ed85f22eebc62aff734
James Robinson <james@a-b.fit>
2020-04-04 19:33:23 -0300

    Added random char order

--- test/earth/bermuda/leetcode/StringToIntegerTest.java
+++ test/earth/bermuda/leetcode/StringToIntegerTest.java
@@ -98,23 +98,99 @@ class StringToIntegerTest {
     }
 
     @Test
-    public void can_test_1000000() {
+    public void can_convert_12() {
+        String target = "-000000000000001";
+        int actual = new StringToInteger().myAtoi(target);
+        int expected = -1;
+        assertEquals(expected, actual, target);
+    }
+
+    @Test
+    public void can_convert_13() {
+        String target = "--0000000000001";
+        int actual = new StringToInteger().myAtoi(target);
+        int expected = 0;
+        assertEquals(expected, actual, target);
+    }
+
+    @Test
+    public void can_convert_14() {
+        String target = "-+0000000000001";
+        int actual = new StringToInteger().myAtoi(target);
+        int expected = 0;
+        assertEquals(expected, actual, target);
+    }
+
+    @Test
+    public void can_convert_15() {
+        String target = "  -0 451";
+        int actual = new StringToInteger().myAtoi(target);
+        int expected = 0;
+        assertEquals(expected, actual, target);
+    }
+
+    @Test
+    public void can_convert_16() {
+        String target = "0-1";
+        int actual = new StringToInteger().myAtoi(target);
+        int expected = 0;
+        assertEquals(expected, actual, target);
+    }
+
+    public static int validate(String str) {
+        int value = 0;
+        boolean negative = false;
+        boolean started = false;
+        for (char c : str.toCharArray()) {
+            if (c == ' ' && !started) {
+                continue;
+            }
+            if (c == '+' && !started) {
+                started = true;
+                continue;
+            } else if (c == '-' && !started) {
+                negative = true;
+                started = true;
+                continue;
+            }
+            started = true;
+            if (c < 48 || c > 57) {
+                break;
+            }
+            if (negative && (value > 214748364 || (value == 214748364 && (c - 48) >= 8))) {
+                return Integer.MIN_VALUE;
+            }
+            if (!negative && (value > 214748364 || (value == 214748364 && (c - 48) >= 7))) {
+                return Integer.MAX_VALUE;
+            }
+            value = value * 10 + (c - 48);
+        }
+        return negative ? -value : value;
+    }
+
+    @Test
+    public void can_test_100_000() {
         StringToInteger solution = new StringToInteger();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 50_000; i++) {
             long target = (long) Math.pow(10d, Math.random() * 11);
             if (Math.random() < .5) {
                 target = -target;
             }
+            String str = target + "";
             int actual = solution.myAtoi(target + "");
-            if (target > (long) Integer.MAX_VALUE) {
-                assertEquals(Integer.MAX_VALUE, actual, target + "");
+            int expected = validate(str);
+            assertEquals(expected, actual, target + "");
+        }
+        String charset = " +-09a";
+        for (int i = 0; i < 50_000; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < (int)(11 * Math.random()); j++) {
+                sb.append(charset.charAt((int)(charset.length() * Math.random())));
             }
-            else if (target < (long) Integer.MIN_VALUE) {
-                assertEquals(Integer.MIN_VALUE, actual, target + "");
-            }
-            else {
-                assertEquals((int) target, actual, target + "");
-            }
+            String str = sb.toString();
+            int actual = solution.myAtoi(str);
+            int expected = validate(str);
+            assertEquals(expected, actual, str);
         }
     }
 
