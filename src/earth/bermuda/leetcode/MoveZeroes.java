57327b5a121a3c07e805bcc7cc578c6c6da77572
James Robinson <james@bermuda.earth>
2020-04-04 13:33:10 -0300

    Attempting to iterate over only once

--- src/earth/bermuda/leetcode/MoveZeroes.java
+++ src/earth/bermuda/leetcode/MoveZeroes.java
@@ -2,6 +2,14 @@ package earth.bermuda.leetcode;
 
 public class MoveZeroes {
     public void moveZeroes(int[] actual) {
-
+        int nonZeroIndex = 0;
+        for (int i = 1; i < actual.length; i++) {
+            if (actual[i] != 0) {
+                actual[nonZeroIndex++] = actual[i];
+            }
+        }
+        for (; nonZeroIndex < actual.length; nonZeroIndex++) {
+            actual[nonZeroIndex] = 0;
+        }
     }
 }
