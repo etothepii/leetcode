848ac750739b066fc74921b587b207972e7cf27e
James Robinson <james@bermuda.earth>
2020-04-05 16:38:46 -0300

    I think this is done, cant think of any edge cases.

--- src/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2.java
+++ src/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2.java
@@ -2,6 +2,13 @@ package earth.bermuda.leetcode;
 
 public class BestTimeToBuyAndSellStock2 {
     public int maxProfit(int[] prices) {
-        return 0;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - prices[i - 1];
+            if (profit > 0) {
+                maxProfit += profit;
+            }
+        }
+        return maxProfit;
     }
 }
--- test/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2Test.java
+++ test/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2Test.java
@@ -1,7 +1,52 @@
 package earth.bermuda.leetcode;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BestTimeToBuyAndSellStock2Test {
+
+    @Test
+    public void can_establish_max_profit_1() {
+        int[] prices = new int[] {7,1,5,3,6,4};
+        int expected = 7;
+        int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
+        assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
+    @Test
+    public void can_establish_max_profit_2() {
+        int[] prices = new int[] {1,2,3,4,5};
+        int expected = 4;
+        int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
+        assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
+    @Test
+    public void can_establish_max_profit_3() {
+        int[] prices = new int[] {7,6,4,3,1};
+        int expected = 0;
+        int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
+        assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
+    @Test
+    public void can_establish_max_profit_4() {
+        int[] prices = new int[] {};
+        int expected = 0;
+        int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
+        assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
+    @Test
+    public void can_establish_max_profit_5() {
+        int[] prices = new int[] {-1,-1, -1};
+        int expected = 0;
+        int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
+        assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
 
 }
