f46fc5fbf450e06de063779f498cba67d5d30819
James Robinson <james@bermuda.earth>
2020-04-06 20:47:44 -0300

    Added first test

---
+++ src/earth/bermuda/leetcode/GroupAnagrams.java
@@ -0,0 +1,11 @@
+package earth.bermuda.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return new ArrayList<>();
+    }
+}
---
+++ test/earth/bermuda/leetcode/GroupAnagramsTest.java
@@ -0,0 +1,34 @@
+package earth.bermuda.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GroupAnagramsTest {
+
+    @Test
+    public void can_group_anagrams_1() {
+        String[] target = new String[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        List<List<String>> actual = new GroupAnagrams().groupAnagrams(target);
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("ate","eat","tea"),
+                Arrays.asList("nat","tan"),
+                Collections.singletonList("bat")
+        );
+        assertEquals(toSet(expected), toSet(actual), Arrays.toString(target));
+    }
+
+    private static Set<Set<String>> toSet(List<List<String>> strings) {
+        Set<Set<String>> sets = new HashSet<>();
+        for (List<String> list : strings) {
+            Set<String> set = new HashSet<>();
+            for (String string : list) {
+                set.add(string);
+            }
+            sets.add(set);
+        }
+        return sets;
+    }
+}
