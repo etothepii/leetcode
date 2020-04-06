6a1573d2fb191c5fd8dccf0e49785cc8deddd4fe
James Robinson <james@bermuda.earth>
2020-04-05 21:01:46 -0300

    I think this is done, cant think of any edge cases.

--- src/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2.java
+++ src/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2.java
@@ -2,13 +2,10 @@ package earth.bermuda.leetcode;
 
 public class BestTimeToBuyAndSellStock2 {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            int profit = prices[i] - prices[i - 1];
-            if (profit > 0) {
-                maxProfit += profit;
-            }
-        }
-        return maxProfit;
+        int sum = 0;
+        for (int i = 1; i < prices.length; i++)
+            if (prices[i] > prices[i - 1])
+                sum += prices[i] - prices[i - 1];
+        return sum;
     }
 }
--- test/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2Test.java
+++ test/earth/bermuda/leetcode/BestTimeToBuyAndSellStock2Test.java
@@ -1,16 +1,29 @@
 package earth.bermuda.leetcode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Timer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BestTimeToBuyAndSellStock2Test {
 
+    public int validate(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - prices[i - 1];
+            if (profit > 0) {
+                maxProfit += profit;
+            }
+        }
+        return maxProfit;
+    }
+
     @Test
     public void can_establish_max_profit_1() {
-        int[] prices = new int[] {7,1,5,3,6,4};
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
         int expected = 7;
         int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
         assertEquals(expected, actual, Arrays.toString(prices));
@@ -18,7 +31,7 @@ class BestTimeToBuyAndSellStock2Test {
 
     @Test
     public void can_establish_max_profit_2() {
-        int[] prices = new int[] {1,2,3,4,5};
+        int[] prices = new int[]{1, 2, 3, 4, 5};
         int expected = 4;
         int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
         assertEquals(expected, actual, Arrays.toString(prices));
@@ -26,7 +39,7 @@ class BestTimeToBuyAndSellStock2Test {
 
     @Test
     public void can_establish_max_profit_3() {
-        int[] prices = new int[] {7,6,4,3,1};
+        int[] prices = new int[]{7, 6, 4, 3, 1};
         int expected = 0;
         int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
         assertEquals(expected, actual, Arrays.toString(prices));
@@ -34,7 +47,7 @@ class BestTimeToBuyAndSellStock2Test {
 
     @Test
     public void can_establish_max_profit_4() {
-        int[] prices = new int[] {};
+        int[] prices = new int[]{};
         int expected = 0;
         int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
         assertEquals(expected, actual, Arrays.toString(prices));
@@ -42,10 +55,42 @@ class BestTimeToBuyAndSellStock2Test {
 
     @Test
     public void can_establish_max_profit_5() {
-        int[] prices = new int[] {-1,-1, -1};
+        int[] prices = new int[]{-1, -1, -1};
         int expected = 0;
         int actual = new BestTimeToBuyAndSellStock2().maxProfit(prices);
         assertEquals(expected, actual, Arrays.toString(prices));
+    }
+
+    @Test
+    public void can_run_100_000_times() {
+        BestTimeToBuyAndSellStock2 solution = new BestTimeToBuyAndSellStock2();
+        for (int i = 0; i < 100_000; i++) {
+            int[] prices = new int[(int) (50 * Math.random())];
+            for (int j = 0; j < prices.length; j++) {
+                prices[j] = (int) (Math.random() * 20000) - 10000;
+            }
+            int expected = validate(prices);
+            int actual = solution.maxProfit(prices);
+            assertEquals(expected, actual, Arrays.toString(prices));
+        }
+    }
+
+    @Test
+    public void can_run_1_000_000_times() {
+        long target = 3_000_000;
+        BestTimeToBuyAndSellStock2 solution = new BestTimeToBuyAndSellStock2();
+        long running = 0;
+        for (int i = 0; i < 100_000; i++) {
+            int[] prices = new int[(int) (50 * Math.random())];
+            for (int j = 0; j < prices.length; j++) {
+                prices[j] = (int) (Math.random() * 20000) - 10000;
+            }
+            long start = System.nanoTime();
+            solution.maxProfit(prices);
+//            validate(prices);
+            running += System.nanoTime() - start;
+        }
+        Assertions.assertTrue(running < target, running + " > " + target);
     }
 
 
