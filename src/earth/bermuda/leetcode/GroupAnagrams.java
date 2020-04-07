bb8aac6302e8ba15f62465b00ad13baefcb3478c
James Robinson <james@bermuda.earth>
2020-04-06 21:46:25 -0300

    Reverted to first solution which is the fastest I can do.

--- src/earth/bermuda/leetcode/GroupAnagrams.java
+++ src/earth/bermuda/leetcode/GroupAnagrams.java
@@ -4,42 +4,19 @@ import java.util.*;
 
 public class GroupAnagrams {
 
-    class Anagram {
-        private final int[] charCount = new int[26];
-
-        public Anagram(String string) {
-            for (char c : string.toCharArray()) {
-                charCount[c - 96]++;
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Anagram anagram = (Anagram) o;
-            return Arrays.equals(charCount, anagram.charCount);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(charCount);
-        }
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> anagrams = new ArrayList<>();
-        Map<Anagram, List<String>> anagramMap = new HashMap<>();
+        HashMap<String, List<String>> strings = new HashMap<>();
         for (String str : strs) {
-            Anagram anagram = new Anagram(str);
-            List<String> strings = anagramMap.get(anagram);
-            if (strings == null) {
-                strings = new ArrayList<>();
-                anagrams.add(strings);
-                anagramMap.put(anagram, strings);
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = String.copyValueOf(chars);
+            List<String> list = strings.get(sorted);
+            if (list == null) {
+                list = new ArrayList<>();
+                strings.put(sorted, list);
             }
-            strings.add(str);
+            list.add(str);
         }
-        return anagrams;
+        return Arrays.asList(strings.values().toArray(new List[0]));
     }
 }
