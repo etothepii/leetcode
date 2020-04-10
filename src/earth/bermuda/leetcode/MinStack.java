835ab0b65d26c285398e01b93682b8712905cf99
James Robinson <james@bermuda.earth>
2020-04-10 11:51:37 -0300

    A naive implementation

--- src/earth/bermuda/leetcode/MinStack.java
+++ src/earth/bermuda/leetcode/MinStack.java
@@ -1,24 +1,34 @@
 package earth.bermuda.leetcode;
 
+import java.util.ArrayList;
+
 public class MinStack {
-    /** initialize your data structure here. */
+
+    private final ArrayList<Integer> list = new ArrayList<>();
+
     public MinStack() {
 
     }
 
     public void push(int x) {
-
+        list.add(x);
     }
 
     public void pop() {
-
+        list.remove(list.size() -1);
     }
 
     public int top() {
-        return 0;
+        return list.get(list.size() - 1);
     }
 
     public int getMin() {
-        return 0;
+        int min = Integer.MAX_VALUE;
+        for (int i : list) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 }
