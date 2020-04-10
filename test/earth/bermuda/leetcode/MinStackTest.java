db74342ff5dd677c25b199d79cb0f80eb9307856
James Robinson <james@bermuda.earth>
2020-04-10 11:46:37 -0300

    Added the example test

---
+++ test/earth/bermuda/leetcode/MinStackTest.java
@@ -0,0 +1,20 @@
+package earth.bermuda.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinStackTest {
+
+    @Test
+    public void can_find_minimum_item_on_stack_1() {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        assertEquals(-3, minStack.getMin());
+        minStack.pop();
+        assertEquals(0, minStack.top());
+        assertEquals(-2, minStack.getMin());
+    }
+}
