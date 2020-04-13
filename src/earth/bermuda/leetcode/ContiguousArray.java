a4360f0329c49806b3d15ebde8fd245a3fa82d61
James Robinson <james@a-b.fit>
2020-04-13 20:54:37 -0300

    Implemented Tests

--- src/earth/bermuda/leetcode/ContiguousArray.java
+++ src/earth/bermuda/leetcode/ContiguousArray.java
@@ -2,6 +2,17 @@ package earth.bermuda.leetcode;
 
 public class ContiguousArray {
     public int findMaxLength(int[] nums) {
-        return 0;
+        int cum = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            cum += nums[i];
+            nums[i] = cum;
+            for (int j = max + 1; i - j * 2 + 1 >= 0; j++) {
+                if (nums[i] - nums[i - j * 2 + 1] == j) {
+                    max = j;
+                }
+            }
+        }
+        return max * 2;
     }
 }
