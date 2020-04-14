d1482b96f732a1bb7546c9d1b4ed0fcd7469be76
James Robinson <james@a-b.fit>
2020-04-13 23:47:29 -0300

    Removed a few more operations

--- src/earth/bermuda/leetcode/ContiguousArray.java
+++ src/earth/bermuda/leetcode/ContiguousArray.java
@@ -7,26 +7,26 @@ public class ContiguousArray {
         char[] firstSeen = new char[100];
         int cum = 0;
         int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
+        for (int i = 0; i < nums.length;) {
+            if (nums[i++] == 0) {
                 cum++;
             }
             else {
                 cum--;
             }
             if (cum == 0) {
-                max = Math.max(max, i + 1);
+                max = Math.max(max, i);
                 continue;
             }
             int index = cum < 0 ? ~ (cum << 1) : cum << 1;
             if (index >= firstSeen.length) {
                 firstSeen = Arrays.copyOf(firstSeen, firstSeen.length * 10);
             }
-            else if (firstSeen[index] > 0) {
-                max = Math.max(max, i + 1 - firstSeen[index]);
+            if (firstSeen[index] > 0) {
+                max = Math.max(max, i - firstSeen[index]);
             }
             else {
-                firstSeen[index] = (char)(i + 1);
+                firstSeen[index] = (char)i;
             }
         }
         return max;
