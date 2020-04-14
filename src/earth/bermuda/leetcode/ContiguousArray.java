1406a6a16aa43ba4738b5ea4b8e5311d996f7a59
James Robinson <james@a-b.fit>
2020-04-14 00:01:42 -0300

    emoving the continue gets me to 5ms

--- src/earth/bermuda/leetcode/ContiguousArray.java
+++ src/earth/bermuda/leetcode/ContiguousArray.java
@@ -8,27 +8,21 @@ public class ContiguousArray {
         int cum = 0;
         int max = 0;
         char c = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                cum++;
-            }
-            else {
-                cum--;
-            }
-            c++;
+        while (c < nums.length) {
+            cum += nums[c++] * 2 - 1;
             if (cum == 0) {
                 max = Math.max(max, c);
-                continue;
-            }
-            int index = cum < 0 ? ~ (cum << 1) : cum << 1;
-            if (index >= firstSeen.length) {
-                firstSeen = Arrays.copyOf(firstSeen, firstSeen.length * 10);
-            }
-            if (firstSeen[index] > 0) {
-                max = Math.max(max, c - firstSeen[index]);
             }
             else {
-                firstSeen[index] = c;
+                int index = cum < 0 ? ~(cum << 1) : cum << 1;
+                if (index >= firstSeen.length) {
+                    firstSeen = Arrays.copyOf(firstSeen, firstSeen.length * 10);
+                }
+                if (firstSeen[index] > 0) {
+                    max = Math.max(max, c - firstSeen[index]);
+                } else {
+                    firstSeen[index] = c;
+                }
             }
         }
         return max;
