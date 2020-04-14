dcafd500d8935dae3f47b49f3ee03dd8831f2a44
James Robinson <james@bermuda.earth>
2020-04-14 19:22:05 -0300

    Actually testing

--- src/earth/bermuda/leetcode/PerformStringShifts.java
+++ src/earth/bermuda/leetcode/PerformStringShifts.java
@@ -1,7 +1,25 @@
 package earth.bermuda.leetcode;
 
+import java.util.ArrayList;
+
 public class PerformStringShifts {
     public String stringShift(String s, int[][] shift) {
-        return null;
+        ArrayList<Character> chars = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            chars.add(c);
+        }
+        for (int i = 0; i < shift.length; i++) {
+            char c = chars.remove(i);
+            if (shift[i][0] == 0) {
+                chars.add(Math.max(0, i - shift[i][1]), c);
+            } else {
+                chars.add(Math.min(chars.size(), i + shift[i][1]), c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : chars) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
--- test/earth/bermuda/leetcode/PerformStringShiftsTest.java
+++ test/earth/bermuda/leetcode/PerformStringShiftsTest.java
@@ -15,7 +15,12 @@ class PerformStringShiftsTest {
             chars.add(c);
         }
         for (int i = 0; i < shift.length; i++) {
-            chars.add(i + shift[i][1] * (shift[i][0] == 0 ? -1 : 1), chars.remove(i));
+            char c = chars.remove(i);
+            if (shift[i][0] == 0) {
+                chars.add(Math.max(0, i - shift[i][1]), c);
+            } else {
+                chars.add(Math.min(chars.size(), i + shift[i][1]), c);
+            }
         }
         StringBuilder sb = new StringBuilder();
         for (Character c : chars) {
@@ -29,7 +34,8 @@ class PerformStringShiftsTest {
         String s = "abc";
         int[][] shift = new int[][]{new int[]{0, 1}, new int[]{1, 2}};
         String expected = "cab";
-        String actual = new PerformStringShifts().stringShift(s, shift);
+        String actual = new PerformStringShifts().stringShift(s, shiftCopy(shift));
+        assertEquals(expected, actual, s, shift);
     }
 
     @Test
@@ -37,38 +43,42 @@ class PerformStringShiftsTest {
         String s = "efgabcd";
         int[][] shift = new int[][]{new int[]{1, 1}, new int[]{1, 1}, new int[]{0, 2}, new int[]{1, 3}};
         String expected = "efgabcd";
-        String actual = new PerformStringShifts().stringShift(s, shift);
+        String actual = new PerformStringShifts().stringShift(s, shiftCopy(shift));
+        assertEquals(expected, actual, s, shift);
     }
 
     @Test
     public void can_run_10_000() {
         PerformStringShifts solution = new PerformStringShifts();
         for (int i = 0; i < 10_000; i++) {
-            String s = randomString(1 + (int)(Math.random() * 99));
-            int[][] shift = randomShift(s.length());
-            int[][] shift_2 = shiftCopy(shift);
-            String expected = validate(s, shift);
-            String actual = solution.stringShift(s, shift_2);
-            if (!expected.equals(actual)) {
-                StringBuilder sb = new StringBuilder(shift.length * 10 + 10);
-                sb.append(s);
-                sb.append(" == ");
-                sb.append(actual);
-                sb.append(" != ");
-                sb.append(expected);
-                sb.append(" after ");
+            String s = randomString(1 + (int) (Math.random() * 99));
+            int[][] shift = randomShift((int) (Math.random() * s.length()));
+            String expected = validate(s, shiftCopy(shift));
+            String actual = solution.stringShift(s, shiftCopy(shift));
+            assertEquals(expected, actual, s, shift);
+        }
+    }
+
+    private void assertEquals(String expected, String actual, String s, int[][] shift) {
+        if (!expected.equals(actual)) {
+            StringBuilder sb = new StringBuilder(shift.length * 10 + 10);
+            sb.append(s);
+            sb.append(" == ");
+            sb.append(actual);
+            sb.append(" != ");
+            sb.append(expected);
+            sb.append(" after ");
+            sb.append("[");
+            for (int[] move : shift) {
                 sb.append("[");
-                for (int[] move : shift) {
-                    sb.append("[");
-                    sb.append(move[0]);
-                    sb.append(", ");
-                    sb.append(move[1]);
-                    sb.append("], ");
-                }
-                sb.setLength(sb.length() - 2);
-                sb.append("]");
-                assertEquals(expected, actual, sb.toString());
+                sb.append(move[0]);
+                sb.append(", ");
+                sb.append(move[1]);
+                sb.append("], ");
             }
+            sb.setLength(sb.length() - 2);
+            sb.append("]");
+            assertEquals(expected, actual, sb.toString());
         }
     }
 
@@ -83,7 +93,7 @@ class PerformStringShiftsTest {
     private int[][] randomShift(int length) {
         int[][] shift = new int[length][];
         for (int i = 0; i < length; i++) {
-            shift[i] = new int[] {(int)(Math.random() * 2), (int)(Math.random() * 101)};
+            shift[i] = new int[]{(int) (Math.random() * 2), (int) (Math.random() * 101)};
         }
         return shift;
     }
@@ -91,7 +101,7 @@ class PerformStringShiftsTest {
     private String randomString(int size) {
         char[] s = new char[size];
         for (int i = 0; i < size; i++) {
-            s[i] = (char)(Math.random() * 26 + 97);
+            s[i] = (char) (Math.random() * 26 + 97);
         }
         return new String(s);
     }
