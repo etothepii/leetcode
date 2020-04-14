986fa7e79120bb55b32344fe83790d1342a7bee7
James Robinson <james@a-b.fit>
2020-04-13 23:16:47 -0300

    Moved to char

--- src/earth/bermuda/leetcode/ContiguousArray.java
+++ src/earth/bermuda/leetcode/ContiguousArray.java
@@ -1,26 +1,27 @@
 package earth.bermuda.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ContiguousArray {
     public int findMaxLength(int[] nums) {
-        short[] firstSeen = new short[50_001];
-        boolean[] seen = new boolean[50_001];
-        firstSeen[25_000] = -1;
-        seen[25_000] = true;
+        char[] firstSeen = new char[10];
         int cum = 0;
         int max = 0;
         for (int i = 0; i < nums.length; i++) {
-            cum += nums[i] * 2 - 1;
-            if (Math.abs(cum) > 25_000) {
-                break;
+            cum += nums[i] == 1 ? 1 : -1;
+            int index = cum < 0 ? ~cum * 2 + 1 : cum * 2;
+            if (index >= firstSeen.length) {
+                firstSeen = Arrays.copyOf(firstSeen, firstSeen.length * 10);
             }
-            if (seen[cum + 25_000]) {
-                max =  Math.max(max, i - firstSeen[cum + 25_000]);
+            if (cum == 0) {
+                max = Math.max(max, i + 1);
+            }
+            else if (firstSeen[index] > 0) {
+                max =  Math.max(max, i + 1 - firstSeen[index]);
             }
             else {
-                firstSeen[cum + 25_000] = (short)i;
-                seen[cum + 25_000] = true;
+                firstSeen[index] = (char)(i + 1);
             }
         }
         return max;
