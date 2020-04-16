b0c078d0f026a78ed38a94563f69beba321d5d14
James Robinson <james@bermuda.earth>
2020-04-15 23:14:12 -0300

    Using the up and down approach

--- src/earth/bermuda/leetcode/ProductOfArrayExceptSelf.java
+++ src/earth/bermuda/leetcode/ProductOfArrayExceptSelf.java
@@ -2,32 +2,20 @@ package earth.bermuda.leetcode;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        long non_zero_sum = 1l;
-        int zeroes = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                zeroes++;
-            }
-            else {
-                non_zero_sum *= num;
-            }
+        if (nums.length == 0) {
+            return new int[0];
         }
-        if (non_zero_sum > (long) Integer.MAX_VALUE) {
-            throw new RuntimeException("An unexpected overflow has occured");
-        }
-        int sum_i = (int) non_zero_sum;
-        int[] output = new int[nums.length];
-        if (zeroes > 1) {
-            return output;
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        up[0] = 1;
+        down[nums.length - 1] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            up[i] = up[i - 1] * nums[i - 1];
+            down[nums.length - 1 - i] = down[nums.length - i] * nums[nums.length - i];
         }
         for (int i = 0; i < nums.length; i++) {
-            if (zeroes == 1) {
-                output[i] = nums[i] == 0 ? sum_i : 0;
-            }
-            else {
-                output[i] = sum_i / nums[i];
-            }
+            nums[i] = up[i] * down[i];
         }
-        return output;
+        return nums;
     }
 }
