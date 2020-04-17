a06ea1a09abed162673b8d35e3f44b365df8eaf7
James Robinson <james@bermuda.earth>
2020-04-16 21:53:32 -0300

    Most cases I can think of

--- src/earth/bermuda/leetcode/ValidParenthesisTesting.java
+++ src/earth/bermuda/leetcode/ValidParenthesisTesting.java
@@ -4,6 +4,7 @@ public class ValidParenthesisTesting {
 
     public boolean checkValidString(String s) {
         byte asterisk = 0;
+        byte open_asterisk = 0;
         byte open = 0;
         for (char c : s.toCharArray()){
             if (c == '(') {
@@ -13,15 +14,14 @@ public class ValidParenthesisTesting {
                 open--;
             }
             else if (c == '*') {
-                if (open == 0) {
-                    open++;
-                }
-                else {
-                    asterisk++;
-                }
+                asterisk++;
             }
-            if (open < -asterisk) {
+            if (open + open_asterisk < -asterisk) {
                 return false;
+            }
+            while (open < asterisk && asterisk > 0) {
+                asterisk--;
+                open_asterisk++;
             }
         }
         return open <= asterisk;
