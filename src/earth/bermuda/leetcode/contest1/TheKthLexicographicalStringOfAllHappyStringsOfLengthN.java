c6af52a9cd81c1e514206c0a51d8dd735f415405
James Robinson <james@bermuda.earth>
2020-04-18 12:58:59 -0300

    Submitted something I think works

--- src/earth/bermuda/leetcode/contest1/TheKthLexicographicalStringOfAllHappyStringsOfLengthN.java
+++ src/earth/bermuda/leetcode/contest1/TheKthLexicographicalStringOfAllHappyStringsOfLengthN.java
@@ -3,7 +3,42 @@ package earth.bermuda.leetcode.contest1;
 public class TheKthLexicographicalStringOfAllHappyStringsOfLengthN {
 
     public String getHappyString(int n, int k) {
-        return "";
+        k--;
+        int binary_part = (int)(Math.pow(2, n - 1));
+        if (k >= binary_part * 3) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder(k);
+        char previous = (new char[]{'a', 'b', 'c'})[k / binary_part];
+        sb.append(previous);
+        while (binary_part > 1) {
+            int remained = k % binary_part;
+            binary_part /= 2;
+            int index = remained / binary_part;
+            if (previous == 'a') {
+                if (index == 0) {
+                    previous = 'b';
+                } else {
+                    previous = 'c';
+                }
+            }
+            else if (previous == 'b') {
+                if (index == 0) {
+                    previous = 'a';
+                } else {
+                    previous = 'c';
+                }
+            }
+            else if (previous == 'c') {
+                if (index == 0) {
+                    previous = 'a';
+                } else {
+                    previous = 'b';
+                }
+            }
+            sb.append(previous);
+        }
+        return sb.toString();
     }
 
 }
